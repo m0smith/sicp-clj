96ce02e202551dae43bd919a64c731b17679ccdb
Matthew O. Smith <matt@taxbit.com>
2021-12-21 15:30:49 -0700

    Added 1 level of nesting for define

--- src/sicp/scheme/core.clj
+++ src/sicp/scheme/core.clj
@@ -2,42 +2,75 @@
    (:refer-clojure :exclude [cond define])
    (:require [clojure.tools.macro :refer [macrolet]]))
 
-(defn define-top-level [n body]
-  `(def ~n ~@body))
-
-#_(defn define-top-level-fn [[n & args] body]
-  (let [define 'define
-        n2 'n2
-        bdy 'bdy]
-    `(macrolet [(~define [n2# & bdy#] 
-                 (let [fn-n# (first n2#)
-                       fn-args# (second n2#)]
-                   (list 'letfn [(apply list fn-n#  (vector fn-args#) bdy#)]))
-                 )]
-               (defn ~n ~(vec args) ~@body))))
-
 
 (defn inner-define-fn? 
   ([] 
    false)
+  ([form] 
+   ;;(println "inner-define-fn?" form)
+   (and (list? form) (= 'define (first form)) (-> form second list?))))
+
+(defn inner-define? 
+  ([] 
+   false)
   ([form]
-   (and (seq form) (= 'define (first form)) (-> form second seq))))
+   (and (list? form) (= 'define (first form)) (-> form second list? not))))
 
 (defn inner-define-fn 
-  [[f [n & args] & body]]
-  `(~n ~(vec args) ~@body))
+  [[_ [n & args] & body]]
+  `(~n ~(vec args) (do+ ~@body)))
 
-(defn define-top-level-fn [[n & args] body]
-  (let [inner-def-fns (apply list (map inner-define-fn (take-while inner-define-fn? body)))
-        b (into '() (drop-while inner-define-fn? body))]
-    `(defn ~n ~(vec args) 
-          (letfn ~(into [] inner-def-fns)
-                ~@b))))
+(defn inner-define
+  [[_ n & body]]
+  `(~n ~@body))
+
+(defn define-let 
+  ([forms] `(let ~(into [] (mapcat inner-define forms))))
+  ([forms inner-forms]
+   `(let ~(into [] (mapcat inner-define forms))
+      (~@inner-forms))))
+
+(defn define-letfn 
+  ([forms] `(letfn ~(into [] (map inner-define-fn forms))))
+  ([forms inner-forms]
+   `(letfn ~(into [] (map inner-define-fn forms))
+      (~@inner-forms))))
+
+(defn define-do 
+  ([forms] `(do+ ~@forms))
+  ([forms inner-forms]
+   ;;(println "do" forms)
+   `(do+ ~@forms
+        (~@inner-forms))))
+
+(defn define-form-type
+  [f]
+  ;;(println "define-form-type" f)
+  (clojure.core/cond (inner-define? f) define-let
+                     (inner-define-fn? f) define-letfn
+                     :else define-do))
+
+(defn add-fn [forms]
+  (cons (define-form-type (first forms)) forms))
+
+(defn define-body [body]
+  (let [parts (reverse (map add-fn (partition-by define-form-type body)))
+        [first-f & first-forms] (first parts)]
+    (reduce (fn [acc [f & forms :as el]] 
+              (f forms acc)) 
+            (first-f first-forms) (next parts))))
 
 (defmacro define [n & body]
   (if (list? n)
-    (define-top-level-fn n body)
-    (define-top-level n body)))
+    `(defn ~(first n) ~(into [] (next n))
+       ~(define-body body))
+    `(def ~n 
+       ~(define-body body))))
 
 (defmacro cond [& exprs]
   (cons 'clojure.core/cond (mapcat (fn [[p e :as args]] (if (= p 'else) (list :else e) args )) exprs)))
+
+(defmacro do+ [& body]
+  (if (< (count body) 2)
+    (first body)
+    `(do ~@body)))
